28cd7d390bf58e69a99eeec7ce79a0997a3207fd
swapnil20222 <117707245+swapnil20222@users.noreply.github.com>
2023-08-02 11:57:18 +0530

    insertion of element.py
    Arrays can only take input at ends we cannot change the value in between so only append fucntion is usable

---
+++ Array/insertion of element.py
@@ -0,0 +1,16 @@
+# first create a insert function
+def insert(arr,elements):
+    arr.append(elements)
+# now create a fucntion to insert the key value at last 
+if __name__ == '__main__':
+# u can take user define input and create and array too
+ arr=[1,213,31441,4643636,1231341423]
+ key=132123
+
+ print("before inserting")
+ print(arr)
+
+# now call the insert fucntion 
+ insert(arr,key)
+ print("after inserting the value")
+ print(arr)
