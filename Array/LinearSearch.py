b8717746ba35eb09b5e21d2136b00c3dd0a298a2
swapnil20222 <117707245+swapnil20222@users.noreply.github.com>
2023-08-03 17:38:56 +0530

    Update LinearSearch.py
    first define a function for linear search ,then we can use while loop and run the function till the length of this list, if the number is found then we can print found and else keep repeating.At last if no number found then it prints Not found

--- Array/LinearSearch.py
+++ Array/LinearSearch.py
@@ -1,1 +1,19 @@
+#Linear search programme
+def linearsearch(list,n):
+ i=0
+
+ while i<len(list):
+   if list[i]==n:
+       return True
+   i=i+1
+ return False
+
+list=[2,31,42,123,32134,2334,1231]
+n=43
+
+
+if linearsearch(list,n):
+    print("Found")
+else:
+    print("Not found")
 
