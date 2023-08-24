a0e20ba5039606b7090d406a1b12616124c075be
swapnil20222 <117707245+swapnil20222@users.noreply.github.com>
2023-08-24 07:39:57 +0530

    Update 2nd highest.py
    for this we first take user input and then sort the list using sort function which makes them in ascending order so we know that second highest will always be at 2nd last place so , run the function till n-1 and then using break function stop the loop. then return the value. U will get the answer value

--- Array/2nd highest.py
+++ Array/2nd highest.py
@@ -1,1 +1,16 @@
+def second_highest(a, n):
+    a.sort()
+    second_hig = 0
+
+    for i in range(n-2, -1, -1):
+        if a[i] != a[n - 1]:
+            second_hig = a[i]
+            break
+
+    return second_hig
+
+a = [12, 35, 1, 10, 348, 1]
+n = len(a)
+answer = second_highest(a, n)
+print("The second highest element in the array is: ", answer)
 
